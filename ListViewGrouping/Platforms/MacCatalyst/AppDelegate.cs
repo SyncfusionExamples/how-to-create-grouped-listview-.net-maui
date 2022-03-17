c0c3141b608285b81bb9db89e780634f28bfa5bb
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2022-03-17 16:15:52 +0530

    Add files via upload

---
+++ ListViewGrouping/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ListViewGrouping;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
